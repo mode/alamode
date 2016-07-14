cbdf7c3ecb6f45ab144678c871b3b4960da1d52e
Franklin Clark <franklin@modeanalytics.com>
2016-07-13 23:43:50 -0700

    Moving css to sass and refactoring gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,48 +2,53 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var pump = require('pump');
 var rename = require('gulp-rename');
-var package = require('./package.json')
+var sass = require('gulp-sass');
+var del = require('del');
+var package = require('./package.json');
 
-gulp.task('compress-gh', function (cb) {
-  pump([
-    gulp.src('alamode.js'),
-    uglify(),
-    rename({
-      basename: 'bundle',
-      suffix: '.min'
-    }),
-    gulp.dest('build/gh')
-  ], cb);
-});
+var paths = {
+  entry: 'alamode.js',
+  scss: 'alamode.scss',
+  s3Dest: 'build/s3/' + package.version,
+  ghDest: 'build/gh',
+};
 
-gulp.task('copy-gh', function (cb) {
+gulp.task('styles', function() {
   pump([
-    gulp.src('alamode.js'),
-    rename('bundle.js'),
-    gulp.dest('build/gh')
+    gulp.src(paths.scss),
+    sass().on('error', sass.logError),
+    gulp.dest(paths.ghDest),
+    gulp.dest(paths.s3Dest)
   ]);
 });
 
-gulp.task('compress-s3', function (cb) {
+gulp.task('min-styles', function() {
   pump([
-    gulp.src('alamode.js'),
-    uglify(),
-    rename({
-      basename: 'bundle',
-      suffix: '.min'
-    }),
-    gulp.dest('build/s3/' + package.version )
-  ], cb);
-});
-
-gulp.task('copy-s3', function (cb) {
-  pump([
-    gulp.src('alamode.js'),
-    rename('bundle.js'),
-    gulp.dest('build/s3/' + package.version)
+    gulp.src(paths.scss),
+    sass({ outputStyle: 'compressed' }).on('error', sass.logError),
+    rename({ suffix: '.min' }),
+    gulp.dest(paths.ghDest),
+    gulp.dest(paths.s3Dest)
   ]);
 });
 
-gulp.task('build-gh', ['copy-gh', 'compress-gh'])
-gulp.task('build-s3', ['copy-s3', 'compress-s3'])
-gulp.task('default', ['build-gh', 'build-s3'])
+gulp.task('scripts', function (cb) {
+  pump([
+    gulp.src(paths.entry),
+    gulp.dest(paths.ghDest),
+    gulp.dest(paths.s3Dest),
+    uglify(),
+    rename({
+      suffix: '.min'
+    }),
+    gulp.dest(paths.ghDest),
+    gulp.dest(paths.s3Dest)
+  ], cb);
+});
+
+gulp.task('clean', function () {
+  return del.sync(['build'], {dot: true});
+});
+
+gulp.task('build', ['styles', 'min-styles', 'scripts'])
+gulp.task('default', ['clean', 'build'])
